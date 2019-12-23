c985d34dfc3dabc5986ffbd4a54bc95a4f834f51
4erem6a <Sviridoff_E@mail.ru>
2019-12-23 07:56:32 +0300

    Add SourcePosition docs

--- src/SourcePosition.ts
+++ src/SourcePosition.ts
@@ -1,5 +1,19 @@
+/**
+ * Represents position of a symbol in a string.
+ */
 export interface SourcePosition {
+  /**
+   * Vertical symbol position, starting from 1.
+   */
   line: number;
+
+  /**
+   * Horizontal symbol position, staring from 1.
+   */
   column: number;
+
+  /**
+   * Absolute symbol position, stating from 0.
+   */
   absolute: number;
 }
